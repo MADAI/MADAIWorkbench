e44e06dddebd8a10940ee19b7fc8231194e55fce
Cory Quammen <cquammen@cs.unc.edu>
2012-06-14 11:30:45 -0400

    Initial skeleton for Ensemble Surface Slicing representation

---
+++ Plugins/EnsembleSurfaceSlicing/vtkEnsembleSurfaceSlicingRepresentation.cxx
@@ -0,0 +1,42 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkEnsembleSurfaceSlicingRepresentation.cxx
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkEnsembleSurfaceSlicingRepresentation.h"
+
+#include "vtkCompositePolyDataMapper2.h"
+#include "vtkInformation.h"
+#include "vtkObjectFactory.h"
+#include "vtkProperty.h"
+#include "vtkPVLODActor.h"
+#include "vtkPVRenderView.h"
+#include "vtkRenderer.h"
+
+vtkStandardNewMacro(vtkEnsembleSurfaceSlicingRepresentation);
+//----------------------------------------------------------------------------
+vtkEnsembleSurfaceSlicingRepresentation::vtkEnsembleSurfaceSlicingRepresentation()
+{
+  this->BackfaceActor = vtkPVLODActor::New();
+  this->BackfaceProperty = vtkProperty::New();
+  this->BackfaceMapper = vtkCompositePolyDataMapper2::New();
+  this->LODBackfaceMapper = vtkCompositePolyDataMapper2::New();
+  this->BackfaceRepresentation = FOLLOW_FRONTFACE;
+
+  // Since we are overriding SetupDefaults(), we need to call it again.
+  this->SetupDefaults();
+}
+
+//----------------------------------------------------------------------------
+vtkEnsembleSurfaceSlicingRepresentation::~vtkEnsembleSurfaceSlicingRepresentation()
+{
+}
---
+++ Plugins/EnsembleSurfaceSlicing/vtkEnsembleSurfaceSlicingRepresentation.h
@@ -0,0 +1,43 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkEnsembleSurfaceSlicingRepresentation.h
+
+  Copyright (c) Kitware, Inc.
+  All rights reserved.
+  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkEnsembleSurfaceSlicingRepresentation
+// .SECTION Description
+// vtkEnsembleSurfaceSlicingRepresentation extends vtkGeometryRepresentation to add
+// support for rendering back and front faces separately (with different
+// visibility and properties).
+
+#ifndef __vtkEnsembleSurfaceSlicingRepresentation_h
+#define __vtkEnsembleSurfaceSlicingRepresentation_h
+
+#include "vtkGeometryRepresentationWithFaces.h"
+
+class VTK_EXPORT vtkEnsembleSurfaceSlicingRepresentation : public vtkGeometryRepresentationWithFaces
+{
+public:
+  static vtkEnsembleSurfaceSlicingRepresentation* New();
+  vtkTypeMacro(vtkEnsembleSurfaceSlicingRepresentation, vtkGeometryRepresentationWithFaces);
+
+//BTX
+protected:
+  vtkEnsembleSurfaceSlicingRepresentation();
+  ~vtkEnsembleSurfaceSlicingRepresentation();
+
+private:
+  vtkEnsembleSurfaceSlicingRepresentation(const vtkEnsembleSurfaceSlicingRepresentation&); // Not implemented
+  void operator=(const vtkEnsembleSurfaceSlicingRepresentation&); // Not implemented
+//ETX
+};
+
+#endif
