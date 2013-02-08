050b0197ae90a52ee331389d22b451f6df4c42c8
Cory Quammen <cquammen@cs.unc.edu>
2013-02-08 10:30:29 -0500

    Added two inputs

--- Plugins/VectorComparison/vtkVectorComparisonGlyphFilter.cxx
+++ Plugins/VectorComparison/vtkVectorComparisonGlyphFilter.cxx
@@ -9,7 +9,8 @@ vtkStandardNewMacro(vtkVectorComparisonGlyphFilter);
 //----------------------------------------------------------------------------
 vtkVectorComparisonGlyphFilter::vtkVectorComparisonGlyphFilter()
 {
-
+  this->SetNumberOfInputPorts(2);
+  this->SetNumberOfOutputPorts(1);
 }
 
 //----------------------------------------------------------------------------
@@ -45,10 +46,16 @@ int vtkVectorComparisonGlyphFilter::RequestUpdateExtent(
 //----------------------------------------------------------------------------
 int vtkVectorComparisonGlyphFilter::FillInputPortInformation(int port, vtkInformation *info)
 {
+#if 0
+  if (port == 1)
+    {
+    info->Set(vtkAlgorithm::INPUT_IS_OPTIONAL(), 0);
+    }
+#endif
+
   if (port == 0 || port == 1)
     {
     info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkDataSet");
-    info->Set(vtkAlgorithm::INPUT_IS_OPTIONAL(), 0);
     return 1;
     }
     
