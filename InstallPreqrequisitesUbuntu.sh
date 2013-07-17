e403d47f4337d637ec56a138ab66ce670cc6a24b
Cory Quammen <cquammen@cs.unc.edu>
2013-07-17 15:54:03 -0400

    Added script to install prerequisites needed for building on Ubuntu

---
+++ InstallPreqrequisitesUbuntu.sh
@@ -0,0 +1,21 @@
+#Author: Cory Quammen <cquammen@cs.unc.edu>
+#Last updated: June 10, 2013
+
+# WARNING: You must run this script with sudo
+#
+# This script installs prerequisites necessary to build the MADAI
+# Workbench on Ubuntu. It was developed and tested under Ubuntu 12.04,
+# but should work with other versions.
+
+apt-get install -y   \
+    build-essential  \
+    git              \
+    libusb-1.0.0-dev \
+    python2.7-dev    \
+    libqt4-dev       \
+    qt4-dev-tools    \
+    libxt-dev        \
+    libboost-dev
+
+# Install CMake 2.8.10 or higher.
+
