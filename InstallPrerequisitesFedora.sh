9c4aed55e3e7f37a25139b598a2a54d7bec78ac4
Cory Quammen <cquammen@cs.unc.edu>
2013-07-19 13:15:42 -0400

    Added script to install prerequisites on Fedora

---
+++ InstallPrerequisitesFedora.sh
@@ -0,0 +1,26 @@
+#Author: Cory Quammen <cquammen@cs.unc.edu>
+#Last updated: July 18, 2013
+
+# WARNING: You must run this script with sudo
+#
+# This script installs prerequisites necessary to build the MADAI
+# Workbench on Ubuntu. It was developed and tested under Fedora 19
+# but should work with other versions.
+
+yum install -y     \
+    git            \
+    wget           \
+    libusb1-devel  \
+    gcc-c++        \
+    python-devel   \
+    qt-devel       \
+    qtwebkit-devel \
+    libXt-devel    \
+    boost-devel    \
+    cmake-gui
+
+# I had to manually set some variables in the VRPN build directory
+#
+# LIBUSB1_INCLUDE_DIR /usr/include/libusb-1.0
+# LIBUSB1_LIBRARY     /usr/lib64/libusb-1.0.so.0
+# LIBUSB1_ROOT_DIR    /usr/lib64
